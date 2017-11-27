94cc8154e6a37d31b4116f74da04d51ae2699f0a
vancisin peter <vancisin.peter@gmail.com>
2017-11-27 22:00:48 +0100

    Add project files.

---
+++ nakovChp17_02_showSubTreeWithNnode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nakovChp17_02_showSubTreeWithNnode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
